0a68f5ca32c4bee38022218cecb20fca79fc8c23
WilliamDevin23 <wdevinsp@gmail.com>
2024-12-19 21:50:57 +0700

    Hi, username!

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,22 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
+	const [username, setUsername] = useState("");
+	
+	const getUsername = async () => {
+		const response = await fetch('/api/me', { method: 'GET'});
+		const res_json = await response.json();
+		setUsername(res_json.username);
+	};
+	
+	useEffect(() => {
+		getUsername();
+	}, []);
+	
   return (
     <div className="flex flex-col justify-center items-center md:ml-[220px] p-8">
-      <h1 className="text-3xl md:text-6xl font-bold">Halo, Admin !</h1>
+      <h1 className="text-3xl md:text-6xl font-bold">Halo, {username} !</h1>
     </div>
   );
 };
